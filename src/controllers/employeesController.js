54af9aefeaa3fc7e577a64b09081175886200d9d
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-09-20 08:56:25 -0300

    feat: implement employee retrieval and editing functionality

--- src/controllers/employeesController.js
+++ src/controllers/employeesController.js
@@ -1,5 +1,17 @@
-const { addEmployee } = require("../models/employeesModel");
+const {
+  addEmployee,
+  getAllEmployees,
+  getEmployee,
+  editEmployee,
+} = require("../models/employeesModel");
 
+// Get all employees
+async function allEmployeesGet(req, res) {
+  const allEmployees = await getAllEmployees();
+  res.render("allEmployees", { allEmployees: allEmployees });
+}
+
+// Create employee
 async function createEmployeeGet(req, res) {
   res.render("employeeForm");
 }
@@ -10,4 +22,23 @@ async function createEmployeePost(req, res) {
   res.redirect("/");
 }
 
-module.exports = { createEmployeeGet, createEmployeePost };
+// Edit employee
+async function editEmployeeGet(req, res) {
+  const employeeId = parseInt(req.params.id);
+  const employee = await getEmployee(employeeId);
+  console.log(employee);
+  res.render("employeeEditForm", { employee: employee });
+}
+
+async function editEmployeePost(req, res) {
+  const updatedEmployee = await editEmployee(parseInt(req.params.id), req.body);
+  res.json(req.body, updatedEmployee);
+}
+
+module.exports = {
+  allEmployeesGet,
+  createEmployeeGet,
+  createEmployeePost,
+  editEmployeeGet,
+  editEmployeePost,
+};
