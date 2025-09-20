c2c12ced2e5d7e94bf2b7a38b62fd4507465f42f
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-09-20 08:58:14 -0300

    feat: add routes for editing and retrieving employee details

--- src/routes/employeesRouter.js
+++ src/routes/employeesRouter.js
@@ -1,19 +1,27 @@
 const e = require("express");
 const { Router } = require("express");
 const {
+  allEmployeesGet,
   createEmployeeGet,
   createEmployeePost,
+  editEmployeeGet,
+  editEmployeePost,
 } = require("../controllers/employeesController");
 const employeesRouter = new Router();
 
 // Get all the employees
+employeesRouter.get("/", allEmployeesGet);
 
 // Create employee
 employeesRouter.get("/create", createEmployeeGet);
 employeesRouter.post("/create", createEmployeePost);
 
 // Edit employee
+employeesRouter.get("/:id/edit", editEmployeeGet);
+employeesRouter.post("/:id/edit", editEmployeePost);
 
 // Delete employee
+// employeesRouter.get("/:id/delete");
+// employeesRouter.post("/:id/delete");
 
 module.exports = employeesRouter;
